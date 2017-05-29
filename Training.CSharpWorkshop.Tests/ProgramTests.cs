14118bb2eb056c21464e051baa5d12850b78e737
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 19:16:52 -0400

    Unit Tests - Rename the test class and add a couple tests

---
+++ Training.CSharpWorkshop.Tests/ProgramTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training.CSharpWorkshop.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [Ignore]
+        [TestMethod]
+        public void ProcessTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetRoleMessageTest()
+        {
+            // Arrange
+            var userName = "Andrew";
+            var expected = "Role: Admin.";
+
+            // Act
+            var actual = Program.GetRoleMessage(userName);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
