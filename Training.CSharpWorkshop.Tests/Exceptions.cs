3aa5f0e5ce27d2634dea24dd3daf96737cab5ea6
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:02:20 -0400

    Exceptions - Try/catch

--- Training.CSharpWorkshop.Tests/Exceptions.cs
+++ Training.CSharpWorkshop.Tests/Exceptions.cs
@@ -30,5 +30,41 @@ namespace Training.CSharpWorkshop.Tests
                     throw new ArgumentException("User does not exist");
             }
         }
+
+        [TestMethod]
+        public void TryCatchTest()
+        {
+            // Arrange
+            var userId = 2;
+            var expected = "Dave";
+
+            // Act
+            string actual = String.Empty;
+
+            try
+            {
+                switch (userId)
+                {
+                    case 1:
+                        actual = "Andrew";
+                        break;
+
+                    case 2:
+                        actual = "Dave";
+                        break;
+
+                    default:
+                        throw new ArgumentException("User does not exist");
+                }
+            }
+
+            // Assert
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
