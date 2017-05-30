68ade568a68cc1d1519d06f7a5ffe583e8645b38
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:12:44 -0400

    While Loops - While Correction: Added using.System.Collections.Generic

--- Training.CSharpWorkshop.Tests/WhileLoops.cs
+++ Training.CSharpWorkshop.Tests/WhileLoops.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Training.CSharpWorkshop.Tests
@@ -6,5 +7,39 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class WhileLoops
     {
+        [TestMethod]
+        public void WhileTest()
+        {
+            // Arrange
+            var list = new List<string>();
+
+            list.Add("Andrew");
+            list.Add("Dave");
+            list.Add("Don");
+            list.Add("Mike");
+            list.Add("Sam");
+            list.Add("Vidya");
+
+            var expected = "Andrew, Dave, Don, Mike, Sam, Vidya";
+
+            // Act
+            var actual = String.Empty;
+
+            // Single letters typically represent indexes
+            // i, j, k are the most common used in that order.
+            var i = 0;
+
+            while (i < list.Count)
+            {
+                actual += list[i] + ", ";
+                i++;
+            }
+
+            actual = actual.Trim();
+            actual = actual.TrimEnd(',');
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
