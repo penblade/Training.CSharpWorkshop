f16f8d7c6be8b694fceb2a1e5b235cfb3029a0a8
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 22:03:22 -0400

    Generics and LINQ – Find - Add Find Unit Tests which are the same as the GetUserByName Unit Tests except the different method call

--- Training.CSharpWorkshop.Tests/GenericsTest.cs
+++ Training.CSharpWorkshop.Tests/GenericsTest.cs
@@ -8,5 +8,34 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class GenericsTest
     {
+        [TestMethod]
+        public void FindUserInTheList()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedId = 0;
+            var expectedName = "Andrew";
+
+            // Act
+            var actual = repository.Find("Andrew");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedId, actual.Id);
+            Assert.AreEqual(expectedName, actual.Name);
+        }
+
+        [TestMethod]
+        public void FindUserNotInTheList()
+        {
+            // Arrange
+            var repository = new Repository();
+
+            // Act
+            var actual = repository.Find("Don");
+
+            // Assert
+            Assert.IsNull(actual);
+        }
     }
 }
