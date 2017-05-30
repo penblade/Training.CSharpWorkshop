4e84e8342a1057e8e7ccf39332c167c122576308
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:37:50 -0400

    Objects - Repository Unit Tests - Create unit tests for the method GetUserByName in the RepositoryTest test class covering a user that should and shouldn't be in the list

--- Training.CSharpWorkshop.Tests/RepositoryTest.cs
+++ Training.CSharpWorkshop.Tests/RepositoryTest.cs
@@ -1,10 +1,38 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Training.CSharpWorkshop.Tests
 {
     [TestClass]
     public class RepositoryTest
     {
+        [TestMethod]
+        public void GetUserByNameForUserInTheList()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedId = 0;
+            var expectedName = "Andrew";
+
+            // Act
+            var actual = repository.GetUserByName("Andrew");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedId, actual.Id);
+            Assert.AreEqual(expectedName, actual.Name);
+        }
+
+        [TestMethod]
+        public void GetUserByNameForUserNotInTheList()
+        {
+            // Arrange
+            var repository = new Repository();
+
+            // Act
+            var actual = repository.GetUserByName("Don");
+
+            // Assert
+            Assert.IsNull(actual);
+        }
     }
 }
