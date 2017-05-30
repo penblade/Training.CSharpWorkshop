3b8786aeb9d3f3d410101e73a5b5bb841f3526a8
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 22:40:58 -0400

    Enum Unit Tests - Add Unit Tests for Admin and Guest user permissions

--- Training.CSharpWorkshop.Tests/EnumTest.cs
+++ Training.CSharpWorkshop.Tests/EnumTest.cs
@@ -6,5 +6,46 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class EnumTest
     {
+        [TestMethod]
+        public void AdminUserPermissions()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedName = "Andrew";
+            var expectedCanInsert = true;
+            var expectedCanDelete = true;
+            var expectedCanFind = true;
+
+            // Act
+            var actual = repository.Find("Andrew");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedName, actual.Name);
+            Assert.AreEqual(expectedCanInsert, actual.CanInsert());
+            Assert.AreEqual(expectedCanDelete, actual.CanDelete());
+            Assert.AreEqual(expectedCanFind, actual.CanFind());
+        }
+
+        [TestMethod]
+        public void GuestUserPermissions()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedName = "Dave";
+            var expectedCanInsert = false;
+            var expectedCanDelete = false;
+            var expectedCanFind = true;
+
+            // Act
+            var actual = repository.Find("Dave");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedName, actual.Name);
+            Assert.AreEqual(expectedCanInsert, actual.CanInsert());
+            Assert.AreEqual(expectedCanDelete, actual.CanDelete());
+            Assert.AreEqual(expectedCanFind, actual.CanFind());
+        }
     }
 }
