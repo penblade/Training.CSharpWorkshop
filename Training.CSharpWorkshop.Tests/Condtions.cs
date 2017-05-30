3d8e037435f84068efe6223f310b37288c21fcf6
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 20:37:45 -0400

    Conditions - If

--- Training.CSharpWorkshop.Tests/Condtions.cs
+++ Training.CSharpWorkshop.Tests/Condtions.cs
@@ -6,5 +6,22 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class Condtions
     {
+        [TestMethod]
+        public void IfTest()
+        {
+            // Arrange
+            var userId = 1;
+            var expected = "Andrew";
+
+            // Act
+            string actual = String.Empty;
+            if (userId == 1)
+            {
+                actual = "Andrew";
+            }
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
