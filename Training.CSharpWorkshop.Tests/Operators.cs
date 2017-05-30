93e81a35cc71e3b4b52a15c15c6a1c1b3af43b9a
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 20:31:00 -0400

    Operators - Comparers

--- Training.CSharpWorkshop.Tests/Operators.cs
+++ Training.CSharpWorkshop.Tests/Operators.cs
@@ -6,5 +6,20 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class Operators
     {
+        [TestMethod]
+        // Equal (==), NotEqual (!=), Remainder (%)
+        // GreaterThan (>), GreaterThanOrEqual (>=),
+        // LessThan (<), LessThanOrEqual (<=).
+        public void Equal()
+        {
+            // Arrange
+            var number = 1;
+
+            // Act
+            var actual = number == 1;
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
     }
 }
