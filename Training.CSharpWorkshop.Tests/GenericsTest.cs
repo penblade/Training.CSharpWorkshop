3fcdf40a16571933a67104f21283160731a43770
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:46:18 -0400

    Generics and LINQ - Create GenericsTest test class

---
+++ Training.CSharpWorkshop.Tests/GenericsTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training.CSharpWorkshop.Tests
+{
+    [TestClass]
+    public class GenericsTest
+    {
+    }
+}
