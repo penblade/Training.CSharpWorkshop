dad9348557f96f560c101c1437c6441669763f06
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 22:59:03 -0400

    Polymorphism - Create GuestUser

---
+++ Training.CSharpWorkshop/GuestUser.cs
@@ -0,0 +1,20 @@
+﻿namespace Training.CSharpWorkshop
+{
+    public class GuestUser : User
+    {
+        public GuestUser()
+        {
+            Role = RoleEnum.Guest;
+        }
+
+        public bool GuestMethodOnly()
+        {
+            return true;
+        }
+
+        public override bool CanInsert()
+        {
+            return true;
+        }
+    }
+}
