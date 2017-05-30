196a359daee3ff2e03c7ea1cd8811e6514260154
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 20:31:46 -0400

    Operators - And

--- Training.CSharpWorkshop.Tests/Operators.cs
+++ Training.CSharpWorkshop.Tests/Operators.cs
@@ -21,5 +21,26 @@ namespace Training.CSharpWorkshop.Tests
             // Assert
             Assert.IsTrue(actual);
         }
+
+        [TestMethod]
+        // BitwiseAnd (&), LogicalAnd (&&).
+        public void LogicalAnd()
+        {
+            // Arrange
+            var number = 1;
+            var text = "file";
+
+            // Act
+
+            // BitwiseAnd: Both cases will be compared.
+            // var actual = number == 0 & text == "file";
+
+            // If the first case is false, only the first case will be compared.
+            // This is considered a short-circuit evaluation.
+            var actual = number == 0 && text == "file";
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
     }
 }
