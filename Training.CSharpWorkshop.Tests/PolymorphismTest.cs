2bd604e90725cb765c2a413ac2fafe9ff21eebe1
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 23:29:26 -0400

    Polymorphism – Unit Tests - In PolymorphismTest, add unit tests for new User methods

--- Training.CSharpWorkshop.Tests/PolymorphismTest.cs
+++ Training.CSharpWorkshop.Tests/PolymorphismTest.cs
@@ -6,5 +6,62 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class PolymorphismTest
     {
+        [TestMethod]
+        public void AdminMethodTest()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedName = "Andrew";
+
+            // Act
+            var actual = repository.Find("Andrew");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedName, actual.Name);
+
+            // Verify the type of object is an AdminUser.
+            Assert.AreEqual(typeof(AdminUser), actual.GetType());
+
+            // Cast User object to AdminUser.
+            AdminUser admin;
+
+            // Will throw an exception if not the correct type.
+            admin = (AdminUser)actual;
+            Assert.IsTrue(admin.AdminMethodOnly());
+
+            // Will change admin to null if not the correct type.
+            admin = actual as AdminUser;
+            Assert.IsTrue(admin.AdminMethodOnly());
+        }
+
+        [TestMethod]
+        public void GuestMethodTest()
+        {
+            // Arrange
+            var repository = new Repository();
+            var expectedName = "Dave";
+
+            // Act
+            var actual = repository.Find("Dave");
+
+            // Assert
+            Assert.IsNotNull(actual);
+            Assert.AreEqual(expectedName, actual.Name);
+
+            // Verify the type of object is an GuestUser.
+            Assert.AreEqual(typeof(GuestUser), actual.GetType());
+
+            // Cast User object to GuestUser.
+            GuestUser guest;
+
+            // Will throw an exception if not the correct type.
+            guest = (GuestUser)actual;
+            Assert.IsTrue(guest.GuestMethodOnly());
+
+            // Will change admin to null if not the correct type.
+            guest = actual as GuestUser;
+            Assert.IsTrue(guest.GuestMethodOnly());
+        }
     }
 }
