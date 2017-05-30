e2227115e82547ce4a3544f3dfae83b95d2a7ae1
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:13:39 -0400

    While Loops - While, continue/break

--- Training.CSharpWorkshop.Tests/WhileLoops.cs
+++ Training.CSharpWorkshop.Tests/WhileLoops.cs
@@ -41,5 +41,56 @@ namespace Training.CSharpWorkshop.Tests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void WhileContinueBreakTest()
+        {
+            // Arrange
+            var list = new List<string>();
+
+            list.Add("Andrew");
+            list.Add("Dave");
+            list.Add("Don");
+            list.Add("Mike");
+            list.Add("Sam");
+            list.Add("Vidya");
+
+            var expected = "Andrew, Don";
+
+            // Act
+            var actual = String.Empty;
+
+            // Single letters typically represent indexes
+            // i, j, k are the most common used in that order.
+            var i = 0;
+
+            while (i < list.Count)
+            {
+                // Do not process this loop any further and continue to the next.
+                if (list[i] == "Dave")
+                {
+                    i++;
+                    continue;
+                }
+                if (list[i] == "Mike")
+                {
+                    i++;
+                    continue;
+                }
+
+                // Do not process this loop any further and exit the while loop
+                // regardless if the condition is met or not.
+                if (list[i] == "Sam") break;
+
+                actual += list[i] + ", ";
+                i++;
+            }
+
+            actual = actual.Trim();
+            actual = actual.TrimEnd(',');
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
