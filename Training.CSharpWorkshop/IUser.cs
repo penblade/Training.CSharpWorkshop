0674104c28f14486b020ff65e17957eac738fd5f
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 23:53:47 -0400

    Interfaces - Create a new class named IUser and change it to an interface

---
+++ Training.CSharpWorkshop/IUser.cs
@@ -0,0 +1,12 @@
+﻿namespace Training.CSharpWorkshop
+{
+    public interface IUser
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        RoleEnum Role { get; set; }
+        bool CanInsert();
+        bool CanDelete();
+        bool CanFind();
+    }
+}
