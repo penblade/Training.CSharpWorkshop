3fb10173ea84b1545687a2fd8b0906223cb1ca47
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 21:01:38 -0400

    Exceptions - Throw Exception

--- Training.CSharpWorkshop.Tests/Exceptions.cs
+++ Training.CSharpWorkshop.Tests/Exceptions.cs
@@ -6,5 +6,29 @@ namespace Training.CSharpWorkshop.Tests
     [TestClass]
     public class Exceptions
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "User does not exist")]
+        public void ThrowExceptionTest()
+        {
+            // Arrange
+            var userId = 3;
+
+            // Act
+            string actual = String.Empty;
+
+            switch (userId)
+            {
+                case 1:
+                    actual = "Andrew";
+                    break;
+
+                case 2:
+                    actual = "Dave";
+                    break;
+
+                default:
+                    throw new ArgumentException("User does not exist");
+            }
+        }
     }
 }
