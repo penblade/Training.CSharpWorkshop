471ff585580ff97eb462ceef5e3325fc360d56f3
Andrew Hinkle <penblade@users.noreply.github.com>
2017-05-29 22:25:36 -0400

    Enum - Add methods to verify against the user's role if the user can add, delete, or find users

--- Training.CSharpWorkshop/User.cs
+++ Training.CSharpWorkshop/User.cs
@@ -12,4 +12,29 @@ namespace Training.CSharpWorkshop
         public string Name { get; set; }
         public RoleEnum Role { get; set; }
     }
+
+    namespace Training.CSharpWorkshop
+    {
+        public class User
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public RoleEnum Role { get; set; }
+
+            public virtual bool CanInsert()
+            {
+                return Role == RoleEnum.Admin;
+            }
+
+            public bool CanDelete()
+            {
+                return Role == RoleEnum.Admin;
+            }
+
+            public bool CanFind()
+            {
+                return Role != RoleEnum.None;
+            }
+        }
+    }
 }
